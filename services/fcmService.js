a5a15a4b4144809ad08a3601c4834a009faa36ae
Salah <Mohamed Magdy@DESKTOP-80U1FBC>
2026-05-31 20:08:25 +0300

    chore: setup production settings, helmet security, and db connection format

--- services/fcmService.js
+++ services/fcmService.js
@@ -5,6 +5,9 @@ if (!admin.apps.length) {
   if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     try {
       const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+      if (serviceAccount.private_key) {
+        serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+      }
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
       });
